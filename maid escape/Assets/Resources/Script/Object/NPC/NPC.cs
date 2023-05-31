15f8c081c1219c7dca4403931b4b0c77a2810f90
DESKTOP-DFIROVA\tjdgn <hvh6634@naver.com>
2023-05-31 15:55:57 +0900

    NPC 작업중..

---
+++ maid escape/Assets/Resources/Script/Object/NPC/NPC.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC : MonoBehaviour
+{
+
+    public void Initialize()
+    { 
+    
+    }
+
+    public void NpcUpdate()
+    { 
+    
+    }
+
+}
