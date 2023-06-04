ea77135758a07f0ccd0cb24f7645765b8769d074
DESKTOP-DFIROVA\tjdgn <hvh6634@naver.com>
2023-06-05 02:50:07 +0900

    NPC 채팅박스 작업 완

--- maid escape/Assets/Resources/Script/Object/NPC/NPC.cs
+++ maid escape/Assets/Resources/Script/Object/NPC/NPC.cs
@@ -8,21 +8,14 @@ namespace MaidEscape.Object
     public class NPC : MonoBehaviour
     {
         [SerializeField]
+        private ChatBox chatBox;        // ChatBox 클래스
+
+        public Collider2D Coll { get; set; }       // 현재 NPC가 지닌 콜라이더의 프로퍼티
 
         private void Awake()
         {
-            ChatBox chatBox = GetComponent<ChatBox>();
-            chatBox.Initialize(this.gameObject.GetComponent<NPC>());
-        }
-
-        public void Initialize()
-        {
-
-        }
-
-        public void NpcUpdate()
-        {
-
+            chatBox.Initialize(this);
+            Coll = GetComponent<Collider2D>();
         }
     }
 }
--- maid escape/Assets/Resources/Script/UI/UIElement/ChatBox.cs
+++ maid escape/Assets/Resources/Script/UI/UIElement/ChatBox.cs
@@ -1,18 +1,29 @@
 using MaidEscape.Object;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 namespace MaidEscape.UIElement
 {
     public class ChatBox : MonoBehaviour
     {
-        /*
-         * 1. ChatBox가 NPC를 따라 다녀야함
-         * 2. 
-         */
+        [SerializeField]
+        private float width;
+
+        [SerializeField]
+        private float height;
 
         private NPC target;     // ChatBox가 따라다닐 NPC
+        private RectTransform rect;
+
+        private void Update()
+        {
+           Vector3 chatBoxPos =     Camera.main.WorldToScreenPoint(new Vector3(target.transform.position.x + width,
+               target.transform.position.y + height, 0));
+
+            this.transform.position = chatBoxPos;
+        }
 
         /// <summary>
         /// ChatBox의 초기 세팅을 담당하는 메서드
@@ -20,7 +31,8 @@ namespace MaidEscape.UIElement
         public void Initialize(NPC target)
         {
             this.target = target;
-            transform.localScale = Vector3.one;
+            transform.localScale = Vector2.one;
+            rect = GetComponent<RectTransform>();
         }
     }
 }
